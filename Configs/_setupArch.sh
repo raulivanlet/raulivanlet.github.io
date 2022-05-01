2277a4ca85eb6c4573c0bb68dd35966223e08054
raulivanlet <raulcalinil@gmail.com>
2022-05-01 10:14:07 +0300

    update

---
+++ Configs/_setupArch.sh
@@ -0,0 +1,151 @@
+#!/bin/bash
+
+
+#--------------------Setting time for dualboot
+sudo timedatectl set-local-rtc 1
+
+
+#--------------------Installing all
+echo "\n\nUpdating..."
+sudo pacman -Syu
+
+
+printf "\n\nDownloading..."
+sudo pacman -S os-prober ntfs-3g xorg-server i3-gaps i3blocks i3status dmenu lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
+sudo pacman -S konsole dolphin git network-manager-applet nftables pulseaudio pulseaudio-alsa kmix notification-daemon dunst 
+sudo pacman -S gnome-keyring libsecret seahorse lm_sensors xdg-utils kde-cli-tools nvidia-settings
+#--------------------Installing grub--------------------os-prober ntfs-3g
+#--------------------Installing Xorg--------------------xorg-server xorg-xrandr mesa
+#--------------------Installing Drivers--------------------xf86-video-amdgpu nvidia nvidia-utils nvidia-prime
+#--------------------Install I3--------------------i3-gaps i3blocks i3status dmenu
+#--------------------Install Display Manager--------------------lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
+#--------------------Install Apps--------------------konsole dolphin git
+#--------------------Network--------------------network-manager-applet nftables
+#--------------------Music--------------------pulseaudio pulseaudio-alsa kmix
+#--------------------KDE Apps Theme--------------------lxappearance breeze breeze-gtk ttf-roboto noto-fonts-emoji
+#--------------------Notification Support--------------------notification-daemon dunst
+#--------------------Keyring--------------------gnome-keyring libsecret seahorse
+#--------------------System Temperature--------------------lm_sensors
+#--------------------Managing Application Extensions--------------------xdg-utils kde-cli-tools   <-----handlr - if it doesn't work
+#--------------------Other Xorg / Nvidia Settings--------------------nvidia-settings
+
+
+
+#--------------------Setting Grub
+printf "\n\nSetting Up Grub..."
+if [[ -f /etc/default/grub ]] ;then
+    sudo rm /etc/default/grub
+fi
+sudo cp grub.txt /etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+
+
+#--------------------Configure I3
+printf "\n\nSetting Up I3..."
+if [[ -f ~/.config/i3/config ]] ;then
+	rm ~/.config/i3/config
+fi
+cp i3_config.txt ~/.config/i3/config
+
+
+
+#--------------------Configure I3 Status
+printf "\n\nSetting Up I3 Bar..."
+if [[ -f ~/.config/i3status/i3status.conf ]] ;then
+	rm ~/.config/i3status/i3status.conf
+fi
+cp i3_status_conf.txt ~/.config/i3status/i3status.conf
+
+
+
+#--------------------Pacman can install 32 bit programs
+printf "\n\nSetting Up Pacman..."
+if [[ -f /etc/pacman.conf ]] ;then
+	sudo rm /etc/pacman.conf
+fi
+sudo cp pacman_conf.txt /etc/pacman.conf
+
+
+
+#--------------------Setting bin folder and paths
+printf "\n\nSetting Up Bin & .profile..."
+if [[ ! -d ~/bin ]] ;then
+    mkdir ~/bin
+fi
+if [[ ! -f ~/.profile ]] ;then
+	cp profile.txt ~/.profile
+fi
+
+
+
+#--------------------Disable Mouse Acceleration
+printf "\n\nSetting Mouse Acceleration..."
+if [[ -f /etc/X11/xorg.conf.d/50-mouse-acceleration.conf ]] ;then
+	sudo rm /etc/X11/xorg.conf.d/50-mouse-acceleration.conf
+fi
+sudo cp x_mouse_accel.txt /etc/X11/xorg.conf.d/50-mouse-acceleration.conf
+
+
+
+#--------------------Setting HDPI Monitor
+printf "\n\nSetting HDPI Monitor..."
+if [[ -f ~/.Xresources ]] ;then
+	rm ~/.Xresources
+fi
+cp x_resources.txt ~/.Xresources
+
+
+
+#--------------------Setting Xorg
+printf "\n\nSetting Xorg..."
+#sudo vim /etc/X11/xorg.conf
+if [[ -f /etc/X11/xorg.conf ]] ;then
+    sudo rm /etc/X11/xorg.conf
+fi
+#sudo cp xorg_config.txt /etc/X11/xorg.conf
+# nvidia-xconfig
+
+
+
+#--------------------Install Display Manager
+printf "\n\nSetting Display Manager..."
+#systemctl enable lightdm.service
+#printf "[Seat:*] greeter-session=lightdm-gtk-greeter"
+#sudo vim /etc/lightdm/lightdm.conf
+
+
+
+
+
+
+
+
+: '
+echo ""
+echo ""
+echo "Cleaning..."
+'
+
+
+
+: << 'COMMENT'
+
+printf "\n\nCleaning..."
+sudo pacman -Scc
+if [[ -f ~/.bash_history ]]
+then
+	rm ~/.bash_history
+fi
+
+if [[ -f ~/.bash_history ]]
+then
+	rm ~/.bash_history
+fi
+
+COMMENT
+
+
+
+#THE END
+printf "\n"
