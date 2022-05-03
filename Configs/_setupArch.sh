195874ef7b8f46608431adcd01a45ef0937f7a24
raul_ivanlet <raulcalinil@gmail.com>
2022-05-03 14:03:28 +0300

    upd

--- Configs/_setupArch.sh
+++ Configs/_setupArch.sh
@@ -112,14 +112,19 @@ sudo cp xorg_config.txt /etc/X11/xorg.conf
 '
 
 
+
 : '
-#--------------------Install Display Manager
-printf "\n\nSetting Display Manager..."
-if [[ -f /etc/lightdm/lightdm.conf ]] ;then
-	systemctl enable lightdm.service
-	sudo cp v_lightdm_config.txt /etc/lightdm/lightdm.conf
+systemctl enable lightdm.service
+if [[ -f /etc/lightdm/lightdm-gtk-greeter.conf ]] ;then
+	sudo rm /etc/lightdm/lightdm-gtk-greeter.conf
+	sudo cp lightdm_greeter_conf.txt /etc/lightdm/lightdm-gtk-greeter.conf
 fi
 '
+#--------------------Install Display Manager
+printf "\n\nSetting Display Manager..."
+if [[ -f /etc/lightdm/lightdm.conf ]] ;then	
+	sudo cp v_lightdm_config.txt /etc/lightdm/lightdm.conf
+fi
 
 
 
